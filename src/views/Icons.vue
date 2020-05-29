3453d11e85c9cbd1e099b6bfbe70aa7056757c4b
collins nyamao <collins.nyamao@strathmore.edu>
2020-05-30 02:02:19 +0300

    test: disable LF edit and sound.wav

--- src/views/Icons.vue
+++ src/views/Icons.vue
@@ -114,15 +114,14 @@
   import VueBarcodeScanner from 'vue-barcode-scanner'
 
   Vue.use(VueClipboard)
-  /*let options = {
+  let options = {
       sound: true, // default is false
       soundSrc: '/static/sound.wav', // default is blank
       sensitivity: 300, // default is 100
       requiredAttr: true, // default is false
-      controlSequenceKeys: ['NumLock', 'Clear'], // default is null
       callbackAfterTimeout: true // default is false
-  }*/
-  Vue.use(VueBarcodeScanner)
+  }
+  Vue.use(VueBarcodeScanner, options)
 
   export default {
       directives: {
